bdbf6f1add2ee4bfa5a834828b2900f6472ffb43
Christopher Pepper <p3p@p3psoft.co.uk>
2019-11-02 01:50:16 +0000

    Improve delay_ns accuracy

--- system/lpc176x/time.h
+++ system/lpc176x/time.h
@@ -5,7 +5,7 @@
 namespace time {
 
   [[gnu::always_inline, gnu::optimize("O3")]] static inline void nop() {
-    __asm__ __volatile__("nop;\n\t":::);
+    __asm__ __volatile__("mov r0, r0;\n\t":::);
   }
 
   [[gnu::always_inline, gnu::optimize("O3")]] static inline void __delay_4cycles(uint32_t cy) { // +1 cycle
@@ -13,7 +13,7 @@ namespace time {
       "  .syntax unified\n\t" // is to prevent CM0,CM1 non-unified syntax
       "1:\n\t"
       "  subs %[cnt],#1\n\t" // 1
-      "  nop\n\t"            // 1
+      "  mov r0, r0\n\t"            // 1
       "  bne 1b\n\t"         // 1 + (1? reload)
       : [cnt]"+r"(cy)   // output: +r means input+output
       :                 // input:
@@ -22,16 +22,17 @@ namespace time {
   }
 
   // Delay in cycles
-  [[gnu::always_inline, gnu::optimize("O3")]] static inline void delay_cycles(uint32_t x) {
+  [[gnu::always_inline, gnu::optimize("03")]] static inline void delay_cycles(uint32_t x) {
     if (__builtin_constant_p(x)) {
-      constexpr uint32_t MAXNOPS = 4;
+      constexpr uint32_t MAXNOPS = 16;
       if (x <= (MAXNOPS)) {
-        switch (x) { case 4: nop(); case 3: nop(); case 2: nop(); case 1: nop(); }
+        switch (x) { case 16: nop(); case 15: nop(); case 14: nop(); case 13: nop(); case 12: nop(); case 11: nop(); case 10: nop(); case  9: nop();
+                     case  8: nop(); case  7: nop(); case  6: nop(); case  5: nop(); case  4: nop(); case  3: nop(); case  2: nop(); case  1: nop(); }
       } else { // because of +1 cycle inside delay_4cycles
-        const uint32_t rem = (x - 1) % (MAXNOPS);
+        const uint32_t rem = (x - 1) % 4;
         switch (rem) { case 3: nop(); case 2: nop(); case 1: nop(); }
-        if ((x = (x - 1) / (MAXNOPS)))
-          __delay_4cycles(x); // if need more then 4 nop loop is more optimal
+        if ((x = (x - 1) / 4))
+          __delay_4cycles(x);
       }
     } else if ((x >>= 2)) __delay_4cycles(x);
   }
